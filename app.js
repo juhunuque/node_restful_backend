b1f46bbc809f71ea586b21772f1b18192c107004
Julio N <juhunuque@gmail.com>
2016-11-20 15:24:56 -0600

    Disabled CORS restriction

--- app.js
+++ app.js
@@ -122,7 +122,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/v1', routes);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/v1/catalog', catalog);
 app.use('/v1/user', user);
 app.use('/v1/material', material);
